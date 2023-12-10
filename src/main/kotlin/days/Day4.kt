4d1575bf0ba77239ea7628fca21ad831e08f5fad
Beinßen, Felix <felix.beinssen@student.hs-anhalt.de>
2023-12-10 21:00:51 +0100

    Add solution and unit tests for Part 2 of Day3 problem

--- src/main/kotlin/days/Day4.kt
+++ src/main/kotlin/days/Day4.kt
@@ -23,6 +23,35 @@ class Day4: Day {
     }
 
     override fun part2(text: String, vararg args: Any): Any {
-        TODO("Not yet implemented")
+        val cards = text.lineSequence().toMutableList()
+        var cardId = 0
+        var num = 0
+        while (cardId < cards.size) {
+            var cardScore = 0
+            val card = cards[cardId].replace("Card *[0-9]*: ".toRegex(), "")
+            val cardNumber = extractCardNumbers(cards[cardId])
+            val numbers = card.split(" | ")
+            val winnerNumbers = numbers[0].split(" ").filter { it.isNotEmpty() }
+            val elveNumbers = numbers[1].split(" ").filter { it.isNotEmpty() }
+
+            for (number in winnerNumbers) {
+                if (elveNumbers.contains(number))
+                    cardScore++
+            }
+            num += cardScore
+            if (cardScore > 0) {
+                for (i in 0..<cardScore) {
+                    cards.add(cards[cardNumber + i])
+                }
+            }
+            cardId++
+        }
+
+        println(num)
+        return cards.size
+    }
+
+    fun extractCardNumbers(card: String): Int {
+        return card.split(":")[0].replace("Card *".toRegex(), "").toInt()
     }
 }
--- src/test/kotlin/days/Day4Test.kt
+++ src/test/kotlin/days/Day4Test.kt
@@ -9,11 +9,31 @@ class Day4Test {
 
     @org.junit.jupiter.api.Test
     fun part1Example() {
-        assertEquals(13, day.part1(readFileFromTestResources("day${index}part1Example.txt")))
+        assertEquals(13, day.part1(readFileFromTestResources("day${index}Example.txt")))
     }
 
     @org.junit.jupiter.api.Test
     fun part1Input() {
         assertEquals(25183, day.part1(readFileFromTestResources("day${index}Input.txt")))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun part2Example() {
+        assertEquals(30, day.part2(readFileFromTestResources("day${index}Example.txt"), 2))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun part2Input() {
+        assertEquals(5667240, day.part2(readFileFromTestResources("day${index}Input.txt"), 2))
+    }
+
+    @org.junit.jupiter.api.Test
+    fun testExtractCardNumber() {
+        assertEquals(1, day.extractCardNumbers("Card 1: 9 2 | 10 1"))
+        assertEquals(2, day.extractCardNumbers("Card      2: 9 2 | 10 1"))
+        assertEquals(3, day.extractCardNumbers("Card      3: 9 2 | 10 1"))
+        assertEquals(23, day.extractCardNumbers("Card      23: 9 2 | 10 1"))
+        assertEquals(333, day.extractCardNumbers("Card    333: 9 2 | 10 1"))
+        assertEquals(3, day.extractCardNumbers("Card 3: 9 2 | 10 1"))
     }
 }
